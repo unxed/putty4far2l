244202b7cd3e80cdeeb1e4d7afac80f837e0f4f4
Ivan Sorokin <unxed@mail.ru>
2022-01-27 20:39:14 +0300

    minor fix

--- windows/make.sh
+++ windows/make.sh
@@ -3,7 +3,7 @@
 # sudo apt install mingw-w64
 #
 # this will build x64 version:
-#make TOOLPATH=x86_64-w64-mingw32- -f Makefile.mgw putty.exe
+#make -j$(nproc --all) TOOLPATH=x86_64-w64-mingw32- -f Makefile.mgw putty.exe
 #
 # this will build x32 version:
 make -j$(nproc --all) TOOLPATH=i686-w64-mingw32- -f Makefile.mgw putty.exe
