deb69dbc19c28c552f11ec5ffd4b2fd10e0a0f52
Ivan Sorokin <unxed@mail.ru>
2024-12-07 22:51:00 +0100

    Imported https://github.com/ivanshatsky/putty4far2l/commit/9d2ec04f84a912dc96be798e47faaf419420cfd3

--- logging.c
+++ logging.c
@@ -81,6 +81,11 @@ void logflush(LogContext *ctx)
             fflush(ctx->lgfp);
 }
 
+LogPolicy *log_get_policy(LogContext *ctx)
+{
+    return ctx->lp;
+}
+
 static void logfopen_callback(void *vctx, int mode)
 {
     LogContext *ctx = (LogContext *)vctx;
@@ -246,26 +251,6 @@ void logevent(LogContext *ctx, const char *event)
     } else {
         logevent_internal(ctx, event);
     }
-}
-
-void logevent_and_free(LogContext *ctx, char *event)
-{
-    logevent(ctx, event);
-    sfree(event);
-}
-
-void logeventvf(LogContext *ctx, const char *fmt, va_list ap)
-{
-    logevent_and_free(ctx, dupvprintf(fmt, ap));
-}
-
-void logeventf(LogContext *ctx, const char *fmt, ...)
-{
-    va_list ap;
-
-    va_start(ap, fmt);
-    logeventvf(ctx, fmt, ap);
-    va_end(ap);
 }
 
 /*
--- windows/make.sh
+++
@@ -1,9 +0,0 @@
-#
-# install compiler first:
-# sudo apt install mingw-w64
-#
-# this will build x64 version:
-#make -j$(nproc --all) TOOLPATH=x86_64-w64-mingw32- -f Makefile.mgw putty.exe
-#
-# this will build x32 version:
-make -j$(nproc --all) TOOLPATH=i686-w64-mingw32- -f Makefile.mgw putty.exe
